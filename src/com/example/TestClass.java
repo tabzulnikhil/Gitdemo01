98b9e19b6474f945aaef1ea83ac1f1c90467daf1
nikhil <nikhiltabzul@gmail.com>
2026-05-05 16:57:08 +0530

    first commit

---
+++ src/com/example/TestClass.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void testMethod() {
+        System.out.println("TestNG Test Running");
+    }
+}
